9b17e9b00b79630794fb714de95257713a081537
EndritIdrizi <eidrizi50@stuy.edu>
2024-09-19 20:52:56 -0400

    finished csv code

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,3 +1,11 @@
+'''
+Endrit Idrizi, Jayden Zhang
+JED
+SoftDev
+K06 -- Divine your destiny/ Reading CSV and weighted distribution/ using open to read a csv file, turning it into a dictionary, and then using a random number generator to get a specific spot
+2024-9-19
+time spent: 1 hour
+'''
 import csv
 import random
 with open('occupations.csv', newline='') as csvfile:
@@ -7,8 +15,16 @@ with open('occupations.csv', newline='') as csvfile:
         #print(row)
         if (row[0] != 'Job Class') and (row[0] != 'Total'):
             dict.update({row[0]:float(row[1])})
-    print(dict);
+    #print(dict);
     
 def randomSelection():
+    x = random.uniform(0.0,99.8)
+    print(x)
+    for key,value in dict.items():
+        x = x - value;
+        if x <= 0:
+            print(key)
+            break
     
+randomSelection()
     
