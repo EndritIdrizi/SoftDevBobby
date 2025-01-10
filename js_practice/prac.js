6b788704f4ee394ebeea2ea530f81d5331008d16
Endrit Idrizi <eidrizi50@stuy.edu>
2025-01-10 11:48:02 -0500

    js prac

--- js_practice/prac.js
+++ js_practice/prac.js
@@ -1,10 +1,7 @@
 const button = document.getElementById('colorButton')
-button.addEventListener('click', changeColor);
-var changeColor = function() {
-  document.body.style.backgroundColor = "#" + "008000";
-}
 document.addEventListener('DOMContentLoaded', function() {
 
-    changeColor
+    button.addEventListener('click', function() {
+        document.body.style.backgroundColor = "#" + "008000";
     });
 });
