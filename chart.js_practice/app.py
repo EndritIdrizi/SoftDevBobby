71be7de2cf34810c3d3ec8ce59b769cde24aa580
Endrit Idrizi <eidrizi50@stuy.edu>
2025-04-09 11:48:48 -0400

    work

--- chart.js_practice/app.py
+++ chart.js_practice/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', __type__ = 'line')
+    return render_template('index.html', __type__ = 'radar', __label_array__ = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], __label__ = 'nice',__data_array__ = [3,17,2,9,12,5])
 
 if __name__ == '__main__':
     app.debug = True
